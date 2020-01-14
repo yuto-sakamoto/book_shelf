24801cbb7c6812ab49f5050fe753addcc2b93d37
yuto sakamoto <y.m.a.c.p.r.o.s.11.9@gmail.com>
2020-01-14 23:47:06 +0900

    rails generate時の初期設定のエラー解消

--- config/application.rb
+++ config/application.rb
@@ -16,7 +16,7 @@ module BookShelf
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
     config.i18n.default_locale = :ja   # the framework and any gems in your application.
 
-    config.generators do |generator|
+    config.generators do |g|
       g.stylesheets     false
       g.javascripts     false
       g.helper          false
@@ -24,14 +24,14 @@ module BookShelf
       g.jbuilder        false
       g.scaffold_stylesheet false
 
-      generator.test_framework :rspec,
+      g.test_framework :rspec,
         fixtures: true,
         view_specs:       false,
         controller_specs: false,
         helper_specs: true,
-        routing_specs: false
+        routing_specs: false,
         request_specs:    true
-      generator.fixture_replacement :factory_bot, dir: "spec/factories"
+      g.fixture_replacement :factory_bot, dir: "spec/factories"
     end
 
     # Settings in config/environments/* take precedence over those specified here.
