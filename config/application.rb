cc0059d41423511955599ef7df0806de7ddd8d8e
yuto sakamoto <y.m.a.c.p.r.o.s.11.9@gmail.com>
2020-01-14 23:43:01 +0900

    rails generate時の初期設定とlocalsの日本語化設定

--- config/application.rb
+++ config/application.rb
@@ -12,15 +12,25 @@ module BookShelf
     config.load_defaults 6.0
     config.time_zone = 'Tokyo'
     config.active_record.default_timezone = :local
+     I18n.enforce_available_locales = false
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
+    config.i18n.default_locale = :ja   # the framework and any gems in your application.
 
     config.generators do |generator|
-
+      g.stylesheets     false
+      g.javascripts     false
+      g.helper          false
+      g.assets          false
+      g.jbuilder        false
+      g.scaffold_stylesheet false
 
       generator.test_framework :rspec,
         fixtures: true,
+        view_specs:       false,
         controller_specs: false,
         helper_specs: true,
         routing_specs: false
+        request_specs:    true
       generator.fixture_replacement :factory_bot, dir: "spec/factories"
     end
 
