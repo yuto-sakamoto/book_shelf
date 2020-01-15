b433a865e4518f386fb4646528f9711df3571df1
yuto sakamoto <y.m.a.c.p.r.o.s.11.9@gmail.com>
2020-01-16 00:48:48 +0900

    deviseでメール認証設定(２段階認証のエラー中)

---
+++ config/initializers/session_store.rb
@@ -0,0 +1,1 @@
+BookShelf::Application.config.session_store :cookie_store, key: 'devise_session', expire_after: 1.month
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_14_143559) do
+ActiveRecord::Schema.define(version: 2020_01_15_153454) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -20,6 +20,11 @@ ActiveRecord::Schema.define(version: 2020_01_14_143559) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
+    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
