257a40f8f08561c6cf1e7e333f7acc6821f8e736
yuto sakamoto <y.m.a.c.p.r.o.s.11.9@gmail.com>
2020-01-17 00:09:52 +0900

    ログイン時にエラーになるのでtrackableを一旦コメントアウト

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable,
+         :recoverable, :rememberable,
+        #  :trackable,
+         :validatable,
          :confirmable #add
 end
