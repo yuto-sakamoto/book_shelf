eef4bc7b5f0206ad7a325c5317468af70324582e
yuto sakamoto <y.m.a.c.p.r.o.s.11.9@gmail.com>
2020-01-16 23:59:37 +0900

    develop用のメール認証機能に伴う修正

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -61,9 +61,10 @@ Rails.application.configure do
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
   # devise メール認証機能追加
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.default_url_options = { host: 'localhost', port: 8080 }
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.delivery_method = :smtp
+  config.action_mailer.delivery_method = :letter_opener_web
   config.action_mailer.smtp_settings = {
     :enable_starttls_auto => true,
     :address => "smtp.gmail.com",
