a71c7eb7cd45f38003bd5e620d54a6fe12a7e0d2
y-sakamo10 <y-sakamoto@gbc.co.jp>
2020-01-16 19:47:38 +0900

    typescript実行確認

--- app/javascript/packs/hello_typescript.ts
+++ app/javascript/packs/hello_typescript.ts
@@ -2,3 +2,20 @@
 // like app/views/layouts/application.html.erb.
 
 console.log('Hello world from typescript');
+class User {
+  public age: number;
+  public familyName: string;
+  public givenName: string;
+  constructor(familyName: string, givenName: string, age: number) {
+    this.age = age;
+    this.familyName = familyName;
+    this.givenName = givenName;
+  }
+}
+
+const user = new User('海老原', '賢次', 44); // 名前と年齢は適当に
+
+const contentsElem = document.getElementById('contents');
+if (!!contentsElem) {
+  contentsElem.innerText = `${user.familyName} ${user.givenName}`;
+}
