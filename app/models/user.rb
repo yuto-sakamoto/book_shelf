6f4b7d53f18f3225313fde03b13c4a9adb0f7576
yuto sakamoto <y.m.a.c.p.r.o.s.11.9@gmail.com>
2020-01-30 23:28:50 +0900

    usearnameでログインできるように

--- app/models/user.rb
+++ app/models/user.rb
@@ -38,5 +38,65 @@ class User < ApplicationRecord
          :recoverable, :rememberable,
          :trackable,
          :validatable,
-         :confirmable # add
+         :confirmable,
+         authentication_keys: [:login]
+
+  validates :username, presence: :true, uniqueness: { case_sensitive: false }
+
+  # validates_format_of :username, with: /^[a-zA-Z0-9_\.]*$/, :multiline => true
+  validate :validate_username
+
+  attr_writer :login
+
+  def login
+    @login || self.username || self.email
+  end
+
+  # def self.find_for_database_authentication(warden_conditions)
+  #   conditions = warden_conditions.dup
+  #   if login = conditions.delete(:login)
+  #     where(conditions.to_h).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+  #   elsif conditions.has_key?(:username) || conditions.has_key?(:email)
+  #     where(conditions.to_h).first
+  #   end
+  # end
+
+
+  #   def self.find_for_database_authentication(warden_conditions)
+  #     conditions = warden_conditions.dup
+  #     if login = conditions.delete(:login).downcase
+  #       where(conditions).where('$or' => [ {:username => /^#{Regexp.escape(login)}$/i}, {:email => /^#{Regexp.escape(login)}$/i} ]).first
+  #     else
+  #       where(conditions).first
+  #     end
+  #   end
+
+  def self.find_for_database_authentication(warden_conditions)
+    conditions = warden_conditions.dup
+    if login = conditions.delete(:login)
+      where(conditions).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+    else
+      where(conditions).first
+    end
+  end
+
+  def self.find_first_by_auth_conditions(warden_conditions)
+    conditions = warden_conditions.dup
+    if login = conditions.delete(:login)
+      where(conditions).where(["lower(username) = :value OR lower(email) = :value", { :value => login.downcase }]).first
+    else
+      if conditions[:username].nil?
+        where(conditions).first
+      else
+        where(username: conditions[:username]).first
+      end
+    end
+  end
+  private
+
+    def validate_username
+      if User.where(email: username).exists?
+        errors.add(:username, :invalid)
+      end
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,8 @@ Rails.application.routes.draw do
   end
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: 'hello#index'
+  # root to: 'hello#index'
+
+  resources :books, only: [:show]
+  root to: 'books#index'
 end
